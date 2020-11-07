1ae199626d1d754c1cb88257b8d566f3423026d8
irosen419 <irosen419@gmail.com>
2020-11-07 15:15:41 -0500

    removed dead code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Intro from './Components/Intro'
 import Works from './Containers/Works'
 import About from './Containers/About'
-// import Footer from './Components/Footer'
 import * as FaIcons from 'react-icons/fa'
 import * as CgIcons from 'react-icons/cg'
-import * as GrIcons from 'react-icons/gr'
 import './App.scss';
 
 function App() {
@@ -14,7 +12,6 @@ function App() {
       <Intro />
       <About />
       <Works />
-      {/* <Footer /> */}
       <div id="social">
         <a href="https://www.linkedin.com/in/irosen419/" target="_blank" rel="noreferrer"><FaIcons.FaLinkedin /></a>
         <a href="https://github.com/irosen419" target="_blank" rel="noreferrer"><FaIcons.FaGithub /></a>
