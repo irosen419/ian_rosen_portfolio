9a03d4cd9ece0ffe0bc509c41ac3b2a8b9bbe2ba
irosen419 <irosen419@gmail.com>
2022-08-10 16:26:22 -0500

    Update tech stack

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -5,19 +5,19 @@ function About() {
                 <h1>About</h1>
                 <p>
                     After graduating from <span>Elon University</span> with a degree in Biology, I spent six years
-                as an educator, both state-side and abroad. Eventually, I decided to take my passions for education,
-                travel and wildlife conservation to the <span>Flatiron School</span> where I gained a new passion
-                for building web applications.
-                <br></br>
+                    as an educator, both state-side and abroad. Eventually, I decided to take my passions for education,
+                    travel and wildlife conservation to the <span>Flatiron School</span> where I gained a new passion
+                    for building web applications.
                     <br></br>
-                It was here that I found the perfect outlet for my <span>creativity</span>.
-                I really enjoy learning from team members and collaborating on projects that hold the
-                <span> user experience</span> above all else.
-                <br></br>
                     <br></br>
-                When I'm not behind the keyboard, you can find me behind a drumset or traveling the world in search of
-                rare and endangered species.
-            </p>
+                    It was here that I found the perfect outlet for my <span>creativity</span>.
+                    I really enjoy learning from team members and collaborating on projects that hold the
+                    <span> user experience</span> above all else.
+                    <br></br>
+                    <br></br>
+                    When I'm not behind the keyboard, you can find me behind a drumset or traveling the world in search of
+                    rare and endangered species.
+                </p>
             </div>
             <div class="snap" id="skills">
                 <h1>Skills</h1>
@@ -26,17 +26,21 @@ function About() {
                         <h2>Proficient</h2>
                         <ul>
                             <li>Javascript</li>
-                            <li>React</li>
+                            <li>Angular 9+</li>
+                            <li>ReactJS</li>
                             <li>Ruby</li>
                             <li>Ruby on Rails</li>
                             <li>HTML</li>
                             <li>CSS</li>
+                            <li>JIRA</li>
                             <li>Git</li>
                         </ul>
                     </div>
                     <div id="exposure">
                         <h2>Exposure</h2>
                         <ul>
+                            <li>Jenkins</li>
+                            <li>InVision/Figma</li>
                             <li>GraphQL</li>
                             <li>AWS - S3</li>
                             <li>PostgreSQL</li>
